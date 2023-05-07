2a0e4e6034dd8da8d2281850a49d633d71e1a528
sheldongomel <existing9@mail.ru>
2023-05-08 01:18:17 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+import path from 'path'; 
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirPath = path.join(__dirname, '/files/');
+const copyPath = path.join(__dirname, '/files-copy/');
 
+fs.readdir(copyPath, (err, files) => {
+  if (err) {return 0};
+  files.forEach((f) => {
+    fs.unlink(copyPath + '/' + f, err => {
+      if (err) throw err;
+    });
+  });
+});
+
+fs.cp(dirPath, 
+      copyPath,
+      {recursive: true}, 
+      function (err) {
+        if (err) throw err;
+        else console.log("success!");
+      });
